0cf074e1e0f63c5c01a1d5a16e4a711371db6c4d
kipkoechke <chepkwonyke1@gmail.com>
2023-04-17 11:53:47 +0300

    Added the start screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:adv_quiz_app/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: StartScreen(),
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen()),
     ),
   ));
 }
- 
