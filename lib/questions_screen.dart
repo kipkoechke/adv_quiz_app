b3380f43d2e577a11033a35e1613a2b74fa07785
kipkoechke <chepkwonyke1@gmail.com>
2023-04-18 09:46:06 +0300

    Styled the buttons

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -15,27 +15,31 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text[0],
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map(
-            (answers) {
-              return AnswerButton(
-                answerText: answers,
-                onTap: () {},
-              );
-            },
-          ),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map(
+              (answers) {
+                return AnswerButton(
+                  answerText: answers,
+                  onTap: () {},
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
