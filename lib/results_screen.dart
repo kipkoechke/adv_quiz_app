2aa7409d7c48235cfee43c03cfb42f286943e1c7
kipkoechke <chepkwonyke1@gmail.com>
2023-04-19 09:21:35 +0300

    Created a map of summary data

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:adv_quiz_app/data/questions.dart';
+
+
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({
     super.key,
@@ -7,6 +10,21 @@ class ResultsScreen extends StatelessWidget {
   });
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
